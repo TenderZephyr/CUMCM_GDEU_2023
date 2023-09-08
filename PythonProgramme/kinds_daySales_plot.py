16afb261108bdcced1e796c0639b5cae1cc3e75c
Jonty <ljt20030312@Outlook.com>
2023-09-08 19:52:15 +0800

    version 2.00 description: 修改了python程序的命名，解决了图表无法显示中文的bug

--- PythonProgramme/kinds_daySales_plot.py
+++ PythonProgramme/kinds_daySales_plot.py
@@ -1,25 +1,22 @@
 '''
 Author: Jonty ljt20030312@Outlook.com
 Date: 2023-09-08 11:21
-LastEditTime: 2023-09-08 14:50
+LastEditTime: 2023-09-08 19:44
 Description: 将六个品类的日销售量通过六张图表显示
 '''
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib
-matplotlib.rcParams['font.sans-serif'] = ['SimSun']     # 显示中文
-matplotlib.rcParams['axes.unicode_minus'] = False
+#plt.rcParams['font.sans-serif'] = ['SimHei']  # 黑体
+plt.rcParams['axes.unicode_minus'] = False    # 解决无法显示符号的问题
+sns.set(font = 'SimHei', style = "whitegrid", font_scale = 1.6)        # 解决Seaborn中文显示问题
 
-# 指定包含Excel文件的文件夹路径
+# 指定包含Excel文件的文件夹路径"
 folder_path = "D:\\桌面\\CUMCM\\classificationFile_sales\\sumSales_day"
 
 # 列出文件夹中的所有文件
 file_names = os.listdir(folder_path)
-
-# 设置Seaborn样式
-sns.set(style="whitegrid")
 
 # 定义一组颜色，用于绘制不同的图表
 colors = sns.color_palette("husl", len(file_names))
@@ -31,18 +28,21 @@ for i, file_name in enumerate(file_names):
         df = pd.read_excel(file_path)
 
         # 创建一个新的图表
-        plt.figure(figsize=(50, 18))
+        plt.figure(figsize=(50, 12))
 
         # 绘制销售数据，使用指定的颜色
         plt.plot(df['销售日期'], df['销量(千克)'], linestyle='-', markersize=8, color=colors[i])
-
+        
+        k = file_name[8 : -8]
+        title = k[5 : ]
+        
         # 设置标题、标签等
-        plt.title(f'{file_name}')
-        plt.xlabel('sale day')
-        plt.ylabel('sale(Kg)')
+        plt.title(title)
+        plt.xlabel('销售日期')
+        plt.ylabel('销量(Kg)')
 
         # 如果需要保存图表到文件，可以使用以下命令
-        plt.savefig(f'D:\\桌面\\CUMCM\\classificationFile_sales\\sumSales_day_plot\\plot_{file_name}.png')
+        plt.savefig(f'D:\\桌面\\CUMCM\\classificationFile_sales\\sumSales_day_plot\\{title}_daySalesPlot.png')
 
         # 显示图表
         #plt.show()
